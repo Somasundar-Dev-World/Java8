ef31d6febbb416c712f2fce026d115646a000926
Somasundar M <somasundar@Somasundars-MBP.fios-router.home>
2018-08-23 22:49:46 -0500

    First commit

---
+++ src/com/practice/java8/newfeatures/lambda/FirstLambdas.java
@@ -0,0 +1,15 @@
+package com.practice.java8.newfeatures.lambda;
+
+public class FirstLambdas {
+
+    public static void main(String[] args) {
+        FirstLambdas lambdas = new FirstLambdas();
+        lambdas.testLambda();
+    }
+
+    public void testLambda()
+    {
+        System.out.println(" Hello world...!");
+    }
+
+}
